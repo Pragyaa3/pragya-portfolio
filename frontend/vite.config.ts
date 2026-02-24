1071c5cc3d253a9c103e811c8cec75df9736f00c
Pragya Hurmade <pragyahurmade2226@gmail.com>
2026-02-24 20:11:54 +0530

    cloudflared

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'all',
+      '.trycloudflare.com',
+      'approaches-explicitly-had-includes.trycloudflare.com'
+    ]
+  }
 })
